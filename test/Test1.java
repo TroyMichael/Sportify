6be4d06d0adfe340720ee4d2c727901ca7e5287a
mod <niklas.fessler@gmail.com>
2015-10-24 16:27:24 +0200

    junit lib path changed build.xml

--- test/Test1.java
+++ test/Test1.java
@@ -1,5 +1,5 @@
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
 
 /**
  * Created by mod on 10/22/15.
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class Test1 {
     @Test
     public void test(){
-        assertEquals(true,true);
+        int i =  2;
+
     }
 }

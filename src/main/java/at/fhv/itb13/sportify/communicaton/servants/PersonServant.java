2defabe372811a23e021d88536056197a5d5e92c
mod <niklas.fessler@gmail.com>
2015-10-28 16:54:35 +0100

    PersonServant created

--- src/main/java/at/fhv/itb13/sportify/communicaton/servants/PersonServant.java
+++ src/main/java/at/fhv/itb13/sportify/communicaton/servants/PersonServant.java
@@ -1,7 +1,9 @@
 package at.fhv.itb13.sportify.communicaton.servants;
 
+import at.fhv.itb13.sportify.application.controller.PersonController;
 import at.fhv.itb13.sportify.communicaton.remote.PersonRemote;
 import at.fhv.itb13.sportify.dataTransfer.dtoInterfaces.PersonDTO;
+import at.fhv.itb13.sportify.model.entities.Person;
 
 import java.rmi.RemoteException;
 
@@ -9,8 +11,11 @@ import java.rmi.RemoteException;
  * Created by Patrick on 28.10.2015.
  */
 public class PersonServant implements PersonRemote {
+    private PersonController _personController;
     @Override
     public void create(PersonDTO personDto) throws RemoteException {
         //TODO
+        _personController = PersonController.getInstance();
+        _personController.create(personDto);
     }
 }

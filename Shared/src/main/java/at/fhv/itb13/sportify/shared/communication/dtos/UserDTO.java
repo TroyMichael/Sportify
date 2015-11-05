22a19bd00707647ff8a421467ab4bd08c238f5e4
Patrick Schedler <patrick_schedler@hotmail.com>
2015-11-05 14:02:52 +0100

    added user dto (incl. impl)

---
+++ Shared/src/main/java/at/fhv/itb13/sportify/shared/communication/dtos/UserDTO.java
@@ -0,0 +1,11 @@
+package at.fhv.itb13.sportify.shared.communication.dtos;
+
+public interface UserDTO {
+    String getName();
+
+    void setName(String name);
+
+    String getPassword();
+
+    void setPassword(String password);
+}

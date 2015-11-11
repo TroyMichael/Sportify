64c8c9d69e492530a1879ca6e34e54f9e7c92342
mod <niklas.fessler@gmail.com>
2015-11-11 09:42:18 +0100

    DepartmentDTO fixes

--- Shared/src/main/java/at/fhv/itb13/sportify/shared/communication/dtos/DepartmentDTO.java
+++ Shared/src/main/java/at/fhv/itb13/sportify/shared/communication/dtos/DepartmentDTO.java
@@ -1,7 +1,16 @@
 package at.fhv.itb13.sportify.shared.communication.dtos;
 
+import java.util.Collection;
+
 /**
  * Created by mod on 11/11/15.
  */
 public interface DepartmentDTO {
+    public String getName();
+    public void setName(String name);
+    public String getDescription();
+    public void setDescription(String description);
+    public Collection<SportDTO> getSports();
+    public void addSport(SportDTO sport);
+    public void removeSport(SportDTO sport);
 }

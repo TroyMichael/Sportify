4437614b6340d949543d52bb194365d02d3b1ef3
Patrick Schedler <patrick_schedler@hotmail.com>
2015-11-18 15:31:20 +0100

    added MatchDAO

--- Server/src/main/java/at/fhv/itb13/sportify/server/database/dao/DepartmentDAO.java
+++ Server/src/main/java/at/fhv/itb13/sportify/server/database/dao/DepartmentDAO.java
@@ -4,6 +4,7 @@ import at.fhv.itb13.sportify.server.database.GenericDAOImpl;
 import at.fhv.itb13.sportify.server.model.Department;
 
 public class DepartmentDAO extends GenericDAOImpl<Department, String> {
+
     public DepartmentDAO() {
         super(Department.class);
     }

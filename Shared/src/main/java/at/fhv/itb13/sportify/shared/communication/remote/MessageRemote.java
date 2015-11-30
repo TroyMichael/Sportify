37ec26b8822d7a0ac7f9722d0ed6be7115beca2a
mod <niklas.fessler@gmail.com>
2015-11-30 09:38:04 +0100

    Merge branch 'master' of https://github.com/TroyMichael/Sportify
    # Conflicts:
    #	Server/src/main/java/at/fhv/itb13/sportify/server/communication/datatransfer/mapper/MatchMapper.java
    #	Server/src/main/java/at/fhv/itb13/sportify/server/communication/datatransfer/mapper/TeamMapper.java
    #	Shared/src/main/java/at/fhv/itb13/sportify/shared/communication/dtos/SimpleTournamentDTO.java

--- Shared/src/main/java/at/fhv/itb13/sportify/shared/communication/remote/MessageRemote.java
+++ Shared/src/main/java/at/fhv/itb13/sportify/shared/communication/remote/MessageRemote.java
@@ -1,12 +1,13 @@
 package at.fhv.itb13.sportify.shared.communication.remote;
 
 import javax.jms.ObjectMessage;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /**
  * Created by KYUSS on 29.11.2015.
  */
-public interface MessageRemote {
+public interface MessageRemote extends Remote{
 
     void getMessage (String queueName) throws RemoteException;
     void sendMessage (String queueName, ObjectMessage objectMessage) throws RemoteException;

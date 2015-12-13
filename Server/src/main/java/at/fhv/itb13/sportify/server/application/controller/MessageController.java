9eda698b3034920b7123aa5a002aa42f561decc6
laooola <patrick_schedler@hotmail.com>
2015-12-13 20:49:55 +0100

    changed server module dependencies

--- Server/src/main/java/at/fhv/itb13/sportify/server/application/controller/MessageController.java
+++ Server/src/main/java/at/fhv/itb13/sportify/server/application/controller/MessageController.java
@@ -5,37 +5,26 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.io.Serializable;
-import java.util.Properties;
 
 /**
  * Created by Caroline on 28.11.2015.
- *
  */
 public class MessageController {
 
     public Serializable getMessage(String username) {
-        Properties env = new Properties();
-        env.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
-        env.setProperty(Context.PROVIDER_URL, "tcp://52.28.97.28:61616");
-        String queueName = username +"Queue";
-        env.setProperty("queue." + queueName, queueName);
-
         Context context = null;
-        ObjectMessage message;
-
         try {
-            context = new InitialContext(env);
-            QueueConnectionFactory cf = (QueueConnectionFactory) context.lookup("ConnectionFactory");
+            context = new InitialContext();
+            QueueConnectionFactory cf = (QueueConnectionFactory) context.lookup("jms/myConnectionFactory");
             Connection con = null;
             try {
                 con = cf.createConnection();
-                Destination dest = (Destination) context.lookup(queueName);
+                Destination dest = (Destination) context.lookup(getQueueName(username));
 
                 Session session = con.createSession(false, Session.AUTO_ACKNOWLEDGE);
                 MessageConsumer cons = session.createConsumer(dest);
                 con.start();
-                message = (ObjectMessage) cons.receive(500);
-                //   System.out.println(message.getText());
+                ObjectMessage message = (ObjectMessage) cons.receive(500);
                 if (message != null) {
                     return message.getObject();
                 }
@@ -65,21 +54,14 @@ public class MessageController {
     }
 
     public void sendMessage(String username, Serializable messageDTO) {
-        Properties env = new Properties();
-        env.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
-        env.setProperty(Context.PROVIDER_URL, "tcp://52.28.97.28:61616");
-
-        String queueName = username + "Queue";
-        env.setProperty("queue." + queueName, queueName);
-
         Context context = null;
         try {
-            context = new InitialContext(env);
-            QueueConnectionFactory cf = (QueueConnectionFactory) context.lookup("ConnectionFactory");
+            context = new InitialContext();
+            QueueConnectionFactory cf = (QueueConnectionFactory) context.lookup("jms/myConnectionFactory");
             Connection con = null;
             try {
                 con = cf.createConnection();
-                Destination dest = (Destination) context.lookup(queueName);
+                Destination dest = (Destination) context.lookup(getQueueName(username));
 
                 Session session = con.createSession(false, Session.AUTO_ACKNOWLEDGE);
                 ObjectMessage message = session.createObjectMessage();
@@ -108,6 +90,10 @@ public class MessageController {
                 }
             }
         }
+    }
+
+    private String getQueueName(String username) {
+        return "jms/queue/" + username + "Queue";
     }
 }
 
